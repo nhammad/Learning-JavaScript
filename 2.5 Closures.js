0d2e2699f36cf08ab090690b78d5a71163f9cb71
nhammad <53377055+nhammad@users.noreply.github.com>
2019-12-01 12:41:47 +0000

    Add files via upload

---
+++ 2.5 Closures.js
@@ -0,0 +1,19 @@
+/*
+
+Declare a function named `expandArray()` that:
+
+* Takes no arguments
+* Contains a single local variable, `myArray`, which points to [1, 1, 1]
+* Returns an anonymous function that directly modifies `myArray` by
+  appending another `1` into it
+* The returned function then returns the value of `myArray`
+
+*/
+
+function expandArray(){
+    var myArray = [1, 1, 1];
+    return function(){
+        myArray[3]=1;
+        return myArray;
+    }
+}
